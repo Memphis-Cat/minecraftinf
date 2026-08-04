719a4014796def34575c1fa0cbedb29c1ee9229c
Memphis-Cat <MemphisCatRBetter.en-us.pt@outlook.com>
2026-08-04 17:21:56 +0100

    Remove obsolete CubeStep profiler redirect on 26.2

--- src/main/java/io/github/opencubicchunks/cubicchunks/mixin/core/common/world/level/cube/status/MixinCubeStep.java
+++ src/main/java/io/github/opencubicchunks/cubicchunks/mixin/core/common/world/level/cube/status/MixinCubeStep.java
@@ -1,21 +1,15 @@
 package io.github.opencubicchunks.cubicchunks.mixin.core.common.world.level.cube.status;
 
-import io.github.opencubicchunks.cc_core.api.CubePos;
 import io.github.opencubicchunks.cubicchunks.world.level.cube.status.CubeStep;
-import net.minecraft.resources.ResourceKey;
-import net.minecraft.util.profiling.jfr.JvmProfiler;
-import net.minecraft.util.profiling.jfr.callback.ProfiledDuration;
-import net.minecraft.world.level.Level;
-import org.spongepowered.asm.mixin.Dynamic;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
+/**
+ * Reserved mixin target for cube-step integration.
+ *
+ * <p>CubeStep is implemented directly on Fabric 26.2 and deliberately omits
+ * vanilla chunk JFR profiling, matching the former redirect that returned
+ * {@code null}. No bytecode injection is required.</p>
+ */
 @Mixin(CubeStep.class)
 public class MixinCubeStep {
-    @Dynamic @Redirect(method = "apply", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/profiling/jfr/JvmProfiler;onChunkGenerate(Lio/github/opencubicchunks/cc_core/api/CubePos;Lnet/minecraft/resources/ResourceKey;Ljava/lang/String;)Lnet/minecraft/util/profiling/jfr/callback/ProfiledDuration;"))
-    private ProfiledDuration cc_onApply_profilerCall(JvmProfiler instance, CubePos cubePos, ResourceKey<Level> levelResourceKey, String s) {
-        // TODO we just hack out the profiler stuff for now
-        return null;
-    }
 }
