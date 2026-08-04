f7b522a172fe8c6e04d60d759bfbc5d6825cf0ac
Memphis-Cat <MemphisCatRBetter.en-us.pt@outlook.com>
2026-08-04 14:40:36 +0100

    Register Fabric DASM interface targets in core mixin config

---
+++ src/main/java/io/github/opencubicchunks/cubicchunks/mixin/core/dasm/MixinFabricDasmInterfaceTargets.java
@@ -0,0 +1,14 @@
+package io.github.opencubicchunks.cubicchunks.mixin.core.dasm;
+
+import org.spongepowered.asm.mixin.Mixin;
+
+/** Fabric DASM routing targets for CubicChunks-owned interfaces. */
+@Mixin(
+    targets = {
+        "io.github.opencubicchunks.cubicchunks.client.renderer.chunk.CCSectionCopy",
+        "io.github.opencubicchunks.cubicchunks.server.level.CloTrackingView"
+    },
+    remap = false
+)
+public interface MixinFabricDasmInterfaceTargets {
+}
