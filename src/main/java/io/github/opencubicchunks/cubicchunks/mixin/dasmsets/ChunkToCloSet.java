cb682d0e4974658d385691afe67a02129e4ca257
Memphis-Cat <MemphisCatRBetter.en-us.pt@outlook.com>
2026-08-04 17:53:07 +0100

    Map ChunkPos packing to CloPos runtime ABI

--- src/main/java/io/github/opencubicchunks/cubicchunks/mixin/dasmsets/ChunkToCloSet.java
+++ src/main/java/io/github/opencubicchunks/cubicchunks/mixin/dasmsets/ChunkToCloSet.java
@@ -69,6 +69,9 @@ public interface ChunkToCloSet extends GlobalSet {
         @FieldToMethodRedirect("z:I")
         native int getZ();
 
+        @MethodRedirect("toLong()J")
+        native long toLong();
+
         @ConstructorToFactoryRedirect("<init>(J)V")
         static native CloPos fromLong(long cloPos);
 
