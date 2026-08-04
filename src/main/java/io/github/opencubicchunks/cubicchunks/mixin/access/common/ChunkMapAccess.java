5b445ee1d01a9c092a5b18e538dc771d845954df
Memphis-Cat <MemphisCatRBetter.en-us.pt@outlook.com>
2026-08-04 13:55:00 +0100

    Remove obsolete ChunkMap getChunks invoker

--- src/main/java/io/github/opencubicchunks/cubicchunks/mixin/access/common/ChunkMapAccess.java
+++ src/main/java/io/github/opencubicchunks/cubicchunks/mixin/access/common/ChunkMapAccess.java
@@ -1,11 +1,8 @@
 package io.github.opencubicchunks.cubicchunks.mixin.access.common;
 
-import net.minecraft.server.level.ChunkHolder;
 import net.minecraft.server.level.ChunkMap;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.gen.Invoker;
 
 @Mixin(ChunkMap.class)
 public interface ChunkMapAccess {
-    @Invoker("getChunks") Iterable<ChunkHolder> cc_invokeGetChunks();
 }
